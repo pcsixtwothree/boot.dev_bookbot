858007c6188bd08e6670cae808da6d2e7dd66caa
pc pilot <p5fsortj5@mozmail.com>
2025-12-17 12:52:15 +0100

    CH2_L5

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import num_words
+
 def get_book_text():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
@@ -5,12 +7,7 @@ def get_book_text():
     #print(file_contents)
 
 def main():
-    get_book_text()
-    print(f"Found {num_words(get_book_text())} total words")
+    text = get_book_text()
+    print(f"Found {num_words(text)} total words")
 
-def num_words(book):
-    words = book.split()
-    return len(words)
-
-    
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def num_words(book):
+    words = book.split()
+    return len(words)
