160dc02152532bd3599f612dc0fb13e996d49920
pc pilot <p5fsortj5@mozmail.com>
2025-12-18 20:56:23 +0100

    final project done still looks like shit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # bookbot
 
 BookBot is my first [Boot.dev](https://www.boot.dev) project!
+
+# to whoever reads this (except me ofc)
+# what the hell are you doing here 
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from stats import num_words, num_char, sorting, abc
+import sys
 
 def get_book_text():
-    with open("books/frankenstein.txt") as f:
+    with open(sys.argv[1]) as f:
         file_contents = f.read()
         return file_contents
     #print(file_contents)
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
     text = get_book_text()
     print(f"Found {num_words(text)} total words")
     #print(num_char(text))
@@ -15,8 +20,8 @@ def main():
     for char_dict in sort_list:
         temp_char = char_dict["char"]
         if temp_char.isalpha():
-            char = char_dict["char"]
-            num = char_dict["num"]
+            #char = char_dict["char"] # not used anymore it's just here to haunt me
+            #num = char_dict["num"]
             print(f"{char_dict["char"]}: {char_dict["num"]}")
 
 main()
